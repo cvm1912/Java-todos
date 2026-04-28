a9b1bf46a1bf7dcdabbccc9dd2ea7782e284ef1b
cvm1912 <rajshivam1912@gmail.com>
2026-04-28 23:56:49 +0530

    feature : day-1 code

---
+++ TodosAPI/src/main/java/org/example/todosapi/TodoController.java
@@ -0,0 +1,21 @@
+package org.example.todosapi;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+
+public class TodoController {
+   private static List<Todo> todos ;
+   //constructor
+   public TodoController(){
+        todos = new ArrayList<>();
+        todos.add(new Todo(1, false, "Study", 101));
+   }
+}
+
+
+
