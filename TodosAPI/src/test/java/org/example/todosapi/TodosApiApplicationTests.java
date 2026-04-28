22ef87d13569c2a780b3c7e9d7b321b749e5723e
cvm1912 <rajshivam1912@gmail.com>
2026-04-28 14:48:31 +0530

    java-project added

---
+++ TodosAPI/src/test/java/org/example/todosapi/TodosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.todosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
