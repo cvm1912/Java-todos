fe0f6a87d42bf6230d275b31e1476d8d22c35109
cvm1912 <rajshivam1912@gmail.com>
2026-04-29 23:05:47 +0530

    added create and get methods

--- TodosAPI/src/main/java/org/example/todosapi/TodoController.java
+++ TodosAPI/src/main/java/org/example/todosapi/TodoController.java
@@ -1,6 +1,9 @@
 package org.example.todosapi;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -14,8 +17,24 @@ public class TodoController {
    public TodoController(){
         todos = new ArrayList<>();
         todos.add(new Todo(1, false, "Study", 101));
+       todos.add(new Todo(2, true, "Reading", 102));
+   }
+
+   @GetMapping("/todos")
+    public List<Todo> getTodos(){
+       return todos;
+   }
+
+   @PostMapping("/todos")
+    public Todo createTodo(@RequestBody Todo newTodo){
+       todos.add(newTodo);
+       return newTodo;
    }
 }
+
+
+
+
 
 
 
