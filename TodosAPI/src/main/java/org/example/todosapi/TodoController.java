14fa037fa9a26b2392d790a22fb9b778a6df3aa9
cvm1912 <rajshivam1912@gmail.com>
2026-04-30 12:51:17 +0530

    added

--- TodosAPI/src/main/java/org/example/todosapi/TodoController.java
+++ TodosAPI/src/main/java/org/example/todosapi/TodoController.java
@@ -9,31 +9,45 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1/todos")
 
 public class TodoController {
-   private static List<Todo> todos ;
-   //constructor
-   public TodoController(){
+    private static List<Todo> todos;
+
+    //constructor
+    public TodoController() {
         todos = new ArrayList<>();
         todos.add(new Todo(1, false, "Study", 101));
-       todos.add(new Todo(2, true, "Reading", 102));
-   }
+        todos.add(new Todo(2, true, "Reading", 102));
+    }
 
-   @GetMapping("/todos")
-    public List<Todo> getTodos(){
-       return ResponseEntity.ok(todos).getBody();
-   }
+    @GetMapping("/")
+    public List<Todo> getTodos() {
+        return ResponseEntity.ok(todos).getBody();
+    }
 
 
-   @PostMapping("/todos")
-   public ResponseEntity<Todo> createTodo(@RequestBody Todo newTodo) {
-       todos.add(newTodo);
-       return ResponseEntity
-               .status(HttpStatus.CREATED)
-               .body(newTodo);
-   }
+    @PostMapping("/create")
+    public ResponseEntity<Todo> createTodo(@RequestBody Todo newTodo) {
+        todos.add(newTodo);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(newTodo);
+    }
+
+
+    @GetMapping("/{todoId}")
+    public ResponseEntity<Todo> getTodoById(@PathVariable Long todoId) {
+        for (Todo todo : todos) {
+            if (todo.getId() == todoId) {
+                return ResponseEntity.ok(todo);
+            }
+
+        }
+        return ResponseEntity.notFound().build();
+    }
+
 }
-
 
 
 
